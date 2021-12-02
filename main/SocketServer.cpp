aa0cf6f6942fd8d2b1353fa25e35964c4a133a4d
Michael Szafranek <faule.aepfel@gmail.com>
2021-12-02 14:02:29 +0100

    SocketServer: use debug, info, warn and err instead of normal printf

--- main/SocketServer.cpp
+++ main/SocketServer.cpp
@@ -24,27 +24,27 @@ extern "C" {
 
 void app_main(void)
 {
-	printf("DuetWebSocketServer\n");
+	info("DuetWebSocketServer\n");
 
 	/* Print chip information */
 	esp_chip_info_t chip_info;
 	esp_chip_info(&chip_info);
 
-	printf("This is ESP8266 chip with %d CPU cores, WiFi, ",
+	info("This is ESP8266 chip with %d CPU cores, WiFi, ",
 			chip_info.cores);
 
-	printf("silicon revision %d, ", chip_info.revision);
+	info("silicon revision %d, ", chip_info.revision);
 
-	printf("%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
+	info("%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
 			(chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
 
 	wifi_init_sta();
 
 	for (int i = 10; i >= 0; i--) {
-		printf("Restarting in %d seconds...\n", i);
-		vTaskDelay(1000 / portTICK_PERIOD_MS);
+		info("Restarting in %d seconds...\n", i);
+		vTaskDelay(4000 / portTICK_PERIOD_MS);
 	}
-	printf("Restarting now.\n");
+	info("Restarting now.\n");
 	fflush(stdout);
 	esp_restart();
 }

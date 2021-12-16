285b04275e7b3fba3cc89589ee43950cddaa12cf
Michael Szafranek <faule.aepfel@gmail.com>
2021-12-17 00:38:31 +0100

    Config, Debug: added debugging code into separate module

--- main/Config.h
+++ main/Config.h
@@ -3,7 +3,9 @@
 #ifndef CONFIG_H_INCLUDED
 #define CONFIG_H_INCLUDED
 
-#include <stdint.h>
+#include <cstdint>
+
+#include "Debug.h"
 
 #define SOCKETSERVER_ENABLE 1
 #define CONNECTION_ENABLE 1
@@ -47,15 +49,5 @@ const uint32_t defaultClockControl = 0x2002;		// 80MHz/3, mark:space 2:1
 const uint8_t Backlog = 8;
 
 #define ARRAY_SIZE(_x) (sizeof(_x)/sizeof((_x)[0]))
-
-#ifdef DEBUG
-#define debug(fmt, args...)	do { printf("DBG %s(%d): " fmt, __FILE__, __LINE__, ##args); } while (0)
-#else
-#define debug(_format, ...)	do {} while(false)
-#endif
-
-#define info(fmt, args...)	do { printf("INFO %s(%d): " fmt, __FILE__, __LINE__, ##args); } while (0)
-#define warn(fmt, args...)	do { printf("WARN %s(%d): " fmt, __FILE__, __LINE__, ##args); } while (0)
-#define err(fmt, args...)	do { printf("ERR %s(%d): " fmt, __FILE__, __LINE__, ##args); } while (0)
 
 #endif

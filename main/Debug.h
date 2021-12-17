6be819aa4afd6c32bbc28f4fdb8f2afb6ccbc102
Michael Szafranek <faule.aepfel@gmail.com>
2021-12-17 10:24:02 +0100

    Debug: added debug module

---
+++ main/Debug.h
@@ -0,0 +1,16 @@
+#ifndef DEBUG_H
+#define DEBUG_H 1
+
+#include <cstdio>
+
+#ifdef DEBUG
+#define debug(fmt, args...)	do { printf("DBG %s(%d): " fmt, __FILE__, __LINE__, ##args); } while (0)
+#else
+#define debug(_format, ...)	do {} while(false)
+#endif
+
+#define info(fmt, args...)	do { printf("INFO %s(%d): " fmt, __FILE__, __LINE__, ##args); } while (0)
+#define warn(fmt, args...)	do { printf("WARN %s(%d): " fmt, __FILE__, __LINE__, ##args); } while (0)
+#define err(fmt, args...)	do { printf("ERR %s(%d): " fmt, __FILE__, __LINE__, ##args); } while (0)
+
+#endif /* ifndef DEBUG_H */

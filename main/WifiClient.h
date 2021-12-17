2899b1995a09f83febc216105e076a1d0c7e15bd
Michael Szafranek <faule.aepfel@gmail.com>
2021-12-17 10:18:13 +0100

    WifiClient: second draft of module

--- main/WifiClient.h
+++ main/WifiClient.h
@@ -1,10 +1,41 @@
-#ifndef STATION_H
-#define STATION_H 1
+#ifndef WIFI_CLIENT_H
+#define WIFI_CLIENT_H 1
 
 
 extern "C" {
 	void wifi_init_sta(void);
 }
 
+#include "freertos/FreeRTOS.h"
+#include "freertos/event_groups.h"
 
-#endif /* ifndef STATION_H */
+#include "WifiConfig.h"
+#include "include/MessageFormats.h"
+
+class WifiClient
+{
+private:
+	WifiState state;
+	WifiConfigData *config;
+
+	int Scan();
+	int GetScanResults();
+public:
+	WifiClient();
+	~WifiClient();
+
+	/* FreeRTOS event group to signal when we are connected*/
+	EventGroupHandle_t s_wifi_event_group;
+	int s_retry_num = 0;
+
+	int Configure(const WifiConfigData *config);
+	WifiConfigData *GetConfig();
+	WifiState GetStatus();
+
+	int Start();
+	int Stop();
+	int Wait();
+	int StartAccessPoint();
+};
+
+#endif /* ifndef WIFI_CLIENT_H */
